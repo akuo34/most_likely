830c14eb9f937e7e9923e7b26ec5eda936180627
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-03-03 14:02:07 -0800

    set up

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3500;
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
