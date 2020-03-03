fe1481b3eefa65c51db9c08fffd89c8216667370
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-03-03 15:20:33 -0800

    install socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express();
+const socketIO = require('socket.io');
+
 const port = process.env.PORT || 3500;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+});
+
+server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
