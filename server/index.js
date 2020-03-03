8925e940fee9675554c14d2169ecd8145327a909
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-03-03 15:02:16 -0800

    change port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3500;
+const port = process.env.PORT || 3500;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
